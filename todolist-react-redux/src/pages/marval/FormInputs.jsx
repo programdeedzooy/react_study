cbbf218057e85f00bb2141626d033dbb3e0a767f
gunalan <programdeedzooy@gmail.com>
2022-04-12 15:50:52 +0530

    full-fill

--- todolist-react-redux/src/pages/marval/FormInputs.jsx
+++ todolist-react-redux/src/pages/marval/FormInputs.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux"
-import { Button, Modal } from "react-bootstrap"
+import { Button, Modal ,Table} from "react-bootstrap"
 import { useForm } from "react-hook-form";
 import { fanDetialUpdate } from "../../redux/reduxSlice/marvalFanDetial"
 import Error from '../../components/Error';
@@ -11,26 +11,27 @@ function FormInputs() {
 
   const [show, setShow] = useState(false);
 
-  const { register, handleSubmit, formState: { errors }, reset, clearErrors } = useForm({defaultValues:{
-    fan:"marval"
-  }});
+  const { register, handleSubmit, formState: { errors }, reset, clearErrors } = useForm({
+    defaultValues: {
+      fan: "marval"
+    }
+  });
 
   const fanDetial = useSelector((state) => state.marvalFanDetial.marvalFanDetialLists)
   const heroDetial = useSelector((state) => state.marvalFanDetial.heroDetails)
-  console.log("heroDetial",heroDetial);
+
   const dispatch = useDispatch()
 
   // let checked = "marval"
 
   const movies = fanDetial.map((val) => val.movie)
-  // console.log("movies", movies);
+
   /**
    * @function onsubmit
    * @param {object} data 
    * sumit the val 
    */
   const onSubmit = (data) => {
-    // console.log("data", data);
     dispatch(fanDetialUpdate(data))
     setShow(false)
     reset()
@@ -59,52 +60,67 @@ function FormInputs() {
   }
 
 
-  const heroName =(
+  const heroName = (
     <select {...register("heroName")}>
-    {heroDetial.map((val)=> <option value={val.heroName}>{val.heroName}</option> )}
-  </select>
+      {heroDetial.map((val) => <option value={val.heroName}>{val.heroName}</option>)}
+    </select>
   )
 
   const character = (
     <select {...register("character")}>
-    {heroDetial.map((val)=> <option value={val.character}>{val.character}</option> )}
-  </select>
+      {heroDetial.map((val) => <option value={val.character}>{val.character}</option>)}
+    </select>
   )
 
   const detials = fanDetial.map((val, index) => {
-    const detial = (<div key={index}>
-      <div>name : {val.name}</div>
-      <div>age : {val.movie}</div>
-      <div>fan : {val.fan}</div>
-      <div>cast : {val.cast}</div>
-      {val.linkMovie !== undefined && <div>linkMovie:{val.linkMovie}</div>}
-      <div>date : {val.date}</div>
-    </div>)
+    const detial = (<tr key={index}>
+      <td> {val.name}</td>
+      <td>{val.movie}</td>
+      <td>{val.heroName}</td>
+      <td>{val.character}</td>
+       <td>{val.linkMovie}</td>
+      <td>{val.date}</td>
+    </tr>)
     return detial
   })
 
-
-  const reentererror = (v) => {
+  /**
+     * @function reEnterError
+     * @param {string} v 
+     * @returns 
+     */
+  const reEnterError = (v) => {
     let flag = false;
     movies.map((val) => {
       if (val === v) {
         flag = true
       }
     })
-    console.log("flag", flag);
     return flag === false
   }
-  // console.log("error", errors);
 
-  if(heroDetial.length===0)
-  {
+  if (heroDetial.length === 0) {
     return <h1>pleasse fill heroName</h1>
   }
 
 
   return (
     <>
-      {detials}
+    <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>name</th>
+      <th>movie</th>
+      <th>heroName</th>
+      <th>character</th>
+      <th>linkMovie</th>
+      <th>date</th>
+    </tr>
+  </thead>
+  <tbody>
+   {detials}
+  </tbody>
+</Table>
       <Button variant="primary" onClick={() => setShow(true)}>
         ADD
       </Button>
@@ -118,22 +134,22 @@ function FormInputs() {
             <div>
               <label htmlFor="">name : </label>
               <input {...register("name", { required: "Please enter Name " })} type="text" />
-              {errors.name!==undefined && <Error errors={errors} value="name" />}
+              {errors.name !== undefined && <Error errors={errors} value="name" />}
             </div>
             <div>
               <label htmlFor="">movie : </label>
               <input {...register("movie", {
                 required: "please give movie name", validate: {
-                  "the movie is alredy selected": v => reentererror(v),
+                  "the movie is alredy selected": v => reEnterError(v),
                 }
               })} type="text" />
-              {errors.movie!==undefined && <Error errors={errors} value="movie" />}
+              {errors.movie !== undefined && <Error errors={errors} value="movie" />}
             </div>
             <div>
               <label htmlFor="">fan : </label>
-              <input {...register("fan")} type="radio" value="marval"/>
+              <input {...register("fan")} type="radio" value="marval" />
               <label htmlFor="">marval </label>
-              <input {...register("fan")} type="radio" value="DC"  />
+              <input {...register("fan")} type="radio" value="DC" />
               <label htmlFor="">DC </label>
             </div>
 
@@ -152,7 +168,7 @@ function FormInputs() {
             <div>
               <label htmlFor="">Date to see movi/serial :     </label>
               <input {...register("date", { required: "please select date" })} type="date" />
-              {errors.date!==undefined && <Error errors={errors} value="date" />}
+              {errors.date !== undefined && <Error errors={errors} value="date" />}
             </div>
             <div>
               <input type="submit" />
--- todolist-react-redux/src/pages/marval/HeroDetails.jsx
+++ todolist-react-redux/src/pages/marval/HeroDetails.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react'
-import {useSelector,useDispatch} from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { useForm } from "react-hook-form";
-import { Button, Modal } from "react-bootstrap"
-import {heroDetailUpdate} from "../../redux/reduxSlice/marvalFanDetial"
+import { Button, Modal,Table } from "react-bootstrap"
+import { heroDetailUpdate } from "../../redux/reduxSlice/marvalFanDetial"
 import Error from '../../components/Error';
 import "./Marval.css"
 function HeroDetails() {
-    const [show, setShow] = useState(false);
-    const { register, handleSubmit, formState: { errors }, reset, clearErrors } = useForm();
- 
-    const heroDetial = useSelector((state) => state.marvalFanDetial.heroDetails)
-    const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
+  const { register, handleSubmit, formState: { errors }, reset, clearErrors } = useForm();
 
-    
-   /**
-   * @function onsubmit
-   * @param {object} data 
-   * sumit the val 
-   */
+  const heroDetial = useSelector((state) => state.marvalFanDetial.heroDetails)
+  const dispatch = useDispatch();
+
+
+  /**
+  * @function onsubmit
+  * @param {object} data 
+  * sumit the val 
+  */
   const onSubmit = (data) => {
-    // console.log("data", data);
     dispatch(heroDetailUpdate(data))
     setShow(false)
     reset()
@@ -29,59 +28,73 @@ function HeroDetails() {
    * @function cancel
    * reset value and close model
    */
-   const cancel = () => {
+  const cancel = () => {
     setShow(false)
     clearErrors()
     reset()
   }
 
-   
-
-
-    const reentererror = (v) => {
-        let flag = false;
-        heroDetial.map((val) => {
-          if (val.heroName === v) {
-            flag = true
-          }
-        })
-        console.log("flag", flag);
-        return flag === false
+  /**
+   * @function reEnterError
+   * @param {string} v 
+   * @returns 
+   */
+  const reEnterError = (v) => {
+    let flag = false;
+    heroDetial.map((val) => {
+      if (val.heroName === v) {
+        flag = true
       }
+    })
+    return flag === false
+  }
 
-    const detials = heroDetial.map((val)=><div className='space' key={val.heroName}>
-    <div>heroName : {val.heroName}</div>
-    <div>character : {val.character}</div>
-  </div>)
-    return (
-        <>
-         {detials}
-            <Button variant="primary" onClick={() => setShow(true)}>
-                ADD
-            </Button>
-            <Modal show={show} onHide={() => setShow(false)}>
-                <Modal.Header >
-                    <Modal.Title>marval fan form</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div>
-                        <input {...register("heroName", { required: "please enter HeroName",validate: {
-                  "the heroName is alredy selected": v => reentererror(v),
-                } })} placeholder="Hero Name" />
-                        {errors.heroName!==undefined && <Error errors={errors} value="heroName" />}
-                        </div>
-                        <div>
-                         <input {...register("character",{required: "Please enter Character",})} placeholder="Character" />
-                         {errors.character!==undefined && <Error errors={errors} value="character" />}
-                         </div>
-                        <input type="submit" />
-                    </form>
-                    <button onClick={() => cancel()} >cancel</button>
-                </Modal.Body>
-            </Modal>
-        </>
-    )
+  const detials = heroDetial.map((val) => <tr key={val.heroName}>
+    <td>heroName : {val.heroName}</td>
+    <td>character : {val.character}</td>
+  </tr>)
+  return (
+    <>
+    <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>heroName</th>
+      <th>character</th>
+    </tr>
+  </thead>
+  <tbody>
+  {detials}
+  </tbody>
+</Table>
+     
+      <Button variant="primary" onClick={() => setShow(true)}>
+        ADD
+      </Button>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header >
+          <Modal.Title>marval fan form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <input {...register("heroName", {
+                required: "please enter HeroName", validate: {
+                  "the heroName is alredy selected": v => reEnterError(v),
+                }
+              })} placeholder="Hero Name" />
+              {errors.heroName !== undefined && <Error errors={errors} value="heroName" />}
+            </div>
+            <div>
+              <input {...register("character", { required: "Please enter Character", })} placeholder="Character" />
+              {errors.character !== undefined && <Error errors={errors} value="character" />}
+            </div>
+            <input type="submit" />
+          </form>
+          <button onClick={() => cancel()} >cancel</button>
+        </Modal.Body>
+      </Modal>
+    </>
+  )
 }
 
 export default HeroDetails
